f1f328cbbdae8005662c5dfbc86d46855799b9d8
Davi Augusto <davi_augusto11@hotmail.com>
2023-12-27 01:51:58 -0300

    Remove chamada para o endpoint do arquivo home

--- components/home/index.js
+++ components/home/index.js
@@ -7,21 +7,6 @@ const HomeContainer = () => {
   const [state, setState] = useState([]);
   const { test } = useTestContext()
 
-  useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=bf1a66b28fd724d2769e4f66cc665bc0",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/JSON",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => setState(data.results))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <div className={style.container}>
       <div>
